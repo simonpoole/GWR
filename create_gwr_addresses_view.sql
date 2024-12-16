a206bda3a873b690b342cb6dd7bf084611361c49
Simon Poole <simon@poole.ch>
2024-12-16 15:05:49 +0100

    Ignore all GWR entries for which doffadr is false

--- create_gwr_addresses_view.sql
+++ create_gwr_addresses_view.sql
@@ -1,3 +1,4 @@
-create materialized view gwr_addresses as select gwr_entrances.egid,egaid,gdekt,ggdenr as gdenr,ggdename as gdename,strname,deinr,dplz4 as plz4,dplzz as plzz,dplzname as plzname,coalesce(dkode,gkode) as e,coalesce(dkodn,gkodn) as n,strsp,esid,gkat,gklas,ST_Transform(ST_GeomFromText('POINT(' || coalesce(dkode,gkode) || ' ' || coalesce(dkodn,gkodn) || ')', 2056), 4326) as loc, gstat from gwr_entrances join gwr_buildings on gwr_entrances.EGID = gwr_buildings.EGID with data;
+drop materialized view gwr_addresses;
+create materialized view gwr_addresses as select gwr_entrances.egid,egaid,gdekt,ggdenr as gdenr,ggdename as gdename,strname,deinr,dplz4 as plz4,dplzz as plzz,dplzname as plzname,coalesce(dkode,gkode) as e,coalesce(dkodn,gkodn) as n,strsp,esid,gkat,gklas,ST_Transform(ST_GeomFromText('POINT(' || coalesce(dkode,gkode) || ' ' || coalesce(dkodn,gkodn) || ')', 2056), 4326) as loc, gstat, doffadr from gwr_entrances join gwr_buildings on gwr_entrances.EGID = gwr_buildings.EGID with data;
 CREATE INDEX gwr_addreses_gdenr_idx ON public.gwr_addresses USING btree (gdenr);
 CREATE INDEX gwr_addresses_idx ON public.gwr_addresses USING gist (loc);
